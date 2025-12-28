2a2c779ef16905049ccab04cb55b190859f83ff7
Mallampeta Pratyusha <69028388+Pratyusha14@users.noreply.github.com>
2025-12-28 17:02:16 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ const Contact = ({ theme }) => {
         </p>
 
         <a
-          href="https://drive.google.com/file/d/1uaxdGnv01WqWYlsS_vZijnUuuKVM-cFC/view?usp=sharing"
+          href="https://drive.google.com/file/d/1yuHPY4CxMwY8KsIFS7h7dOy6ySZUWRPC/view?usp=sharing"
           target="_blank"
           rel="noreferrer"
           className="font-bold border-2 border-teal-500 px-4 py-2"
